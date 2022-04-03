974f0b8e342a31c810f2fa01d362cd11d751d820
ColaxDev <colax.dev@gmail.com>
2022-04-03 15:57:40 +0200

    Introduce TokenKind::End

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ enum Expr {
 
 enum Error {
     UnexpectedToken(TokenKind, TokenKind),
-    UnexpectedEOF(TokenKind),
 }
 
 impl Expr {
@@ -36,7 +35,10 @@ impl Expr {
                                 Err(Error::UnexpectedToken(TokenKind::CloseParen, t.kind))
                             }
                         } else {
-                            Err(Error::UnexpectedEOF(TokenKind::CloseParen))
+                            Err(Error::UnexpectedToken(
+                                TokenKind::CloseParen,
+                                TokenKind::End,
+                            ))
                         }
                     } else {
                         Ok(Expr::Sym(name.text))
@@ -45,7 +47,7 @@ impl Expr {
                 _ => Err(Error::UnexpectedToken(TokenKind::Sym, name.kind)),
             }
         } else {
-            Err(Error::UnexpectedEOF(TokenKind::Sym))
+            Err(Error::UnexpectedToken(TokenKind::Sym, TokenKind::End))
         }
     }
 
@@ -235,6 +237,7 @@ enum TokenKind {
     CloseParen,
     Comma,
     Equals,
+    End,
 }
 
 impl fmt::Display for TokenKind {
@@ -246,6 +249,7 @@ impl fmt::Display for TokenKind {
             CloseParen => write!(f, "')'"),
             Comma => write!(f, "','"),
             Equals => write!(f, "'='"),
+            End => write!(f, "end of input"),
         }
     }
 }
@@ -331,9 +335,6 @@ fn main() {
             Ok(expr) => println!("{}", swap.apply_all(&expr)),
             Err(Error::UnexpectedToken(expected, actual)) => {
                 println!("ERROR: expected {} but got {}", expected, actual)
-            }
-            Err(Error::UnexpectedEOF(expected)) => {
-                println!("ERROR: expected {} but got nothing", expected)
             }
         }
     }
