7f8ec8bab229c82e39f72ac3ca9ccac266ce0da5
ColaxDev <colax.dev@gmail.com>
2022-04-05 15:54:48 +0200

    Add support for binary operators

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
 use std::collections::HashMap;
-use std::env;
-use std::fmt;
-use std::fs;
-use std::io::Write;
 use std::io::{stdin, stdout};
+use std::io::Write;
+use std::fmt;
+use std::env;
+use std::fs;
 
 mod lexer;
 
 use lexer::*;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Op {
     Add,
     Sub,
     Mul,
     Div,
     Pow,
+}
+
+impl Op {
+    const MAX_PRECEDENCE: usize = 2;
+
+    fn from_token_kind(kind: TokenKind) -> Option<Self> {
+        match kind {
+            TokenKind::Plus => Some(Op::Add),
+            TokenKind::Dash => Some(Op::Sub),
+            TokenKind::Asterisk => Some(Op::Mul),
+            TokenKind::Slash => Some(Op::Div),
+            TokenKind::Caret => Some(Op::Pow),
+            _ => None
+        }
+    }
+
+    fn precedence(&self) -> usize {
+        use Op::*;
+        match self {
+            Add | Sub => 0,
+            Mul | Div => 1,
+            Pow       => 2,
+        }
+    }
 }
 
 impl fmt::Display for Op {
@@ -51,10 +75,7 @@ enum Error {
 
 impl Expr {
     fn var_or_sym_based_on_name(name: &str) -> Expr {
-        let x = name
-            .chars()
-            .next()
-            .expect("Empty names are not allowed. This might be a bug in the lexer.");
+        let x = name.chars().next().expect("Empty names are not allowed. This might be a bug in the lexer.");
         if x.is_uppercase() || x == '_' {
             Expr::Var(name.to_string())
         } else {
@@ -62,13 +83,13 @@ impl Expr {
         }
     }
 
-    fn parse_fun_args(lexer: &mut Lexer<impl Iterator<Item = char>>) -> Result<Vec<Self>, Error> {
+    fn parse_fun_args(lexer: &mut Lexer<impl Iterator<Item=char>>) -> Result<Vec<Self>, Error> {
         use TokenKind::*;
         let mut args = Vec::new();
         expect_token_kind(lexer, TokenKindSet::single(OpenParen))?;
         if lexer.peek_token().kind == CloseParen {
             lexer.next_token();
-            return Ok(args);
+            return Ok(args)
         }
         args.push(Self::parse(lexer)?);
         while lexer.peek_token().kind == Comma {
@@ -79,21 +100,62 @@ impl Expr {
         if close_paren.kind == CloseParen {
             Ok(args)
         } else {
-            Err(Error::UnexpectedToken(
-                TokenKindSet::single(CloseParen),
-                close_paren,
-            ))
+            Err(Error::UnexpectedToken(TokenKindSet::single(CloseParen), close_paren))
         }
     }
 
-    pub fn parse(lexer: &mut Lexer<impl Iterator<Item = char>>) -> Result<Self, Error> {
-        let mut head = Self::var_or_sym_based_on_name(
-            &expect_token_kind(lexer, TokenKindSet::single(TokenKind::Ident))?.text,
-        );
+    fn parse_fun_or_var_or_sym(lexer: &mut Lexer<impl Iterator<Item=char>>) -> Result<Self, Error> {
+        let mut head = {
+            let token = lexer.peek_token().clone();
+            match token.kind {
+                TokenKind::OpenParen => {
+                    lexer.next_token();
+                    let result = Self::parse(lexer)?;
+                    expect_token_kind(lexer, TokenKindSet::single(TokenKind::CloseParen))?;
+                    result
+                }
+
+                TokenKind::Ident => {
+                    lexer.next_token();
+                    Self::var_or_sym_based_on_name(&token.text)
+                },
+
+                _ => return Err(Error::UnexpectedToken(TokenKindSet::single(TokenKind::OpenParen).set(TokenKind::Ident), token))
+            }
+        };
+
         while lexer.peek_token().kind == TokenKind::OpenParen {
             head = Expr::Fun(Box::new(head), Self::parse_fun_args(lexer)?)
         }
         Ok(head)
+    }
+
+    fn parse_binary_operator(lexer: &mut Lexer<impl Iterator<Item=char>>, current_precedence: usize) -> Result<Self, Error> {
+        if current_precedence > Op::MAX_PRECEDENCE {
+            return Self::parse_fun_or_var_or_sym(lexer)
+        }
+
+        let mut result = Self::parse_binary_operator(lexer, current_precedence + 1)?;
+
+        while let Some(op) = Op::from_token_kind(lexer.peek_token().kind) {
+            if current_precedence != op.precedence() {
+                break
+            }
+
+            lexer.next_token();
+
+            result = Expr::Op(
+                op,
+                Box::new(result),
+                Box::new(Self::parse_binary_operator(lexer, current_precedence)?)
+            );
+        }
+
+        Ok(result)
+    }
+
+    pub fn parse(lexer: &mut Lexer<impl Iterator<Item=char>>) -> Result<Self, Error> {
+        Self::parse_binary_operator(lexer, 0)
     }
 }
 
@@ -104,16 +166,34 @@ impl fmt::Display for Expr {
             Expr::Fun(head, args) => {
                 write!(f, "{}(", head)?;
                 for (i, arg) in args.iter().enumerate() {
-                    if i > 0 {
-                        write!(f, ", ")?
-                    }
+                    if i > 0 { write!(f, ", ")? }
                     write!(f, "{}", arg)?;
                 }
                 write!(f, ")")
-            }
+            },
             Expr::Op(op, lhs, rhs) => {
                 // TODO: different spacing and parenthesis based on the precedence
-                write!(f, "{} {} {}", lhs, op, rhs)
+                match **lhs {
+                    Expr::Op(sub_op, _, _) => if sub_op.precedence() < op.precedence() {
+                        write!(f, "({})", lhs)?
+                    } else {
+                        write!(f, "{}", lhs)?
+                    }
+                    _ => write!(f, "{}", lhs)?
+                }
+                if op.precedence() == 0 {
+                    write!(f, " {} ", op)?;
+                } else {
+                    write!(f, "{}", op)?;
+                }
+                match **rhs {
+                    Expr::Op(sub_op, _, _) => if sub_op.precedence() < op.precedence() {
+                        write!(f, "({})", rhs)
+                    } else {
+                        write!(f, "{}", rhs)
+                    }
+                    _ => write!(f, "{}", rhs)
+                }
             }
         }
     }
@@ -122,7 +202,7 @@ impl fmt::Display for Expr {
 enum Action {
     #[allow(dead_code)]
     Skip,
-    Apply,
+    Apply
 }
 
 enum State {
@@ -186,15 +266,16 @@ impl Strategy for ApplyDeep {
 
 impl Rule {
     fn apply(&self, expr: &Expr, strategy: &mut impl Strategy) -> Expr {
-        fn apply_to_subexprs(
-            rule: &Rule,
-            expr: &Expr,
-            strategy: &mut impl Strategy,
-        ) -> (Expr, bool) {
+        fn apply_to_subexprs(rule: &Rule, expr: &Expr, strategy: &mut impl Strategy) -> (Expr, bool) {
             use Expr::*;
             match expr {
                 Sym(_) | Var(_) => (expr.clone(), false),
-                Op(_, _, _) => todo!("Applying rules to operators"),
+                Op(op, lhs, rhs) => {
+                    let (new_lhs, halt) = apply_impl(rule, lhs, strategy);
+                    if halt { return (Op(*op, Box::new(new_lhs), rhs.clone()), true) }
+                    let (new_rhs, halt) = apply_impl(rule, rhs, strategy);
+                    (Op(*op, Box::new(new_lhs), Box::new(new_rhs)), halt)
+                },
                 Fun(head, args) => {
                     let (new_head, halt) = apply_impl(rule, head, strategy);
                     if halt {
@@ -256,7 +337,13 @@ fn substitute_bindings(bindings: &Bindings, expr: &Expr) -> Expr {
             }
         }
 
-        Op(_, _, _) => todo!("Substituting variables for operators"),
+        Op(op, lhs, rhs) => {
+            Op(
+                *op,
+                Box::new(substitute_bindings(bindings, lhs)),
+                Box::new(substitute_bindings(bindings, rhs))
+            )
+        },
 
         Fun(head, args) => {
             let new_head = substitute_bindings(bindings, head);
@@ -269,10 +356,7 @@ fn substitute_bindings(bindings: &Bindings, expr: &Expr) -> Expr {
     }
 }
 
-fn expect_token_kind(
-    lexer: &mut Lexer<impl Iterator<Item = char>>,
-    kinds: TokenKindSet,
-) -> Result<Token, Error> {
+fn expect_token_kind(lexer: &mut Lexer<impl Iterator<Item=char>>, kinds: TokenKindSet) -> Result<Token, Error> {
     let token = lexer.next_token();
     if kinds.contains(token.kind) {
         Ok(token)
@@ -287,7 +371,9 @@ fn pattern_match(pattern: &Expr, value: &Expr) -> Option<Bindings> {
     fn pattern_match_impl(pattern: &Expr, value: &Expr, bindings: &mut Bindings) -> bool {
         use Expr::*;
         match (pattern, value) {
-            (Sym(name1), Sym(name2)) => name1 == name2,
+            (Sym(name1), Sym(name2)) => {
+                name1 == name2
+            }
             (Var(name), _) => {
                 if name == "_" {
                     true
@@ -297,6 +383,9 @@ fn pattern_match(pattern: &Expr, value: &Expr) -> Option<Bindings> {
                     bindings.insert(name.clone(), value.clone());
                     true
                 }
+            }
+            (Op(op1, lhs1, rhs1), Op(op2, lhs2, rhs2)) => {
+                *op1 == *op2 && pattern_match_impl(lhs1, lhs2, bindings) && pattern_match_impl(rhs1, rhs2, bindings)
             }
             (Fun(name1, args1), Fun(name2, args2)) => {
                 if pattern_match_impl(name1, name2, bindings) && args1.len() == args2.len() {
@@ -309,7 +398,7 @@ fn pattern_match(pattern: &Expr, value: &Expr) -> Option<Bindings> {
                 } else {
                     false
                 }
-            }
+            },
             _ => false,
         }
     }
@@ -332,10 +421,7 @@ struct Context {
 }
 
 impl Context {
-    fn process_command(
-        &mut self,
-        lexer: &mut Lexer<impl Iterator<Item = char>>,
-    ) -> Result<(), Error> {
+    fn process_command(&mut self, lexer: &mut Lexer<impl Iterator<Item=char>>) -> Result<(), Error> {
         let expected_tokens = TokenKindSet::empty()
             .set(TokenKind::Rule)
             .set(TokenKind::Shape)
@@ -348,11 +434,7 @@ impl Context {
             TokenKind::Rule => {
                 let name = expect_token_kind(lexer, TokenKindSet::single(TokenKind::Ident))?;
                 if let Some(existing_rule) = self.rules.get(&name.text) {
-                    return Err(Error::RuleAlreadyExists(
-                        name.text,
-                        name.loc,
-                        existing_rule.loc.clone(),
-                    ));
+                    return Err(Error::RuleAlreadyExists(name.text, name.loc, existing_rule.loc.clone()))
                 }
                 let head = Expr::parse(lexer)?;
                 expect_token_kind(lexer, TokenKindSet::single(TokenKind::Equals))?;
@@ -366,17 +448,16 @@ impl Context {
             }
             TokenKind::Shape => {
                 if let Some(_) = self.current_expr {
-                    return Err(Error::AlreadyShaping(keyword.loc));
+                    return Err(Error::AlreadyShaping(keyword.loc))
                 }
 
                 let expr = Expr::parse(lexer)?;
                 println!(" => {}", &expr);
                 self.current_expr = Some(expr);
-            }
+            },
             TokenKind::Apply => {
                 if let Some(expr) = &self.current_expr {
-                    let strategy_name =
-                        expect_token_kind(lexer, TokenKindSet::single(TokenKind::Ident))?;
+                    let strategy_name = expect_token_kind(lexer, TokenKindSet::single(TokenKind::Ident))?;
 
                     let expected_kinds = TokenKindSet::empty()
                         .set(TokenKind::Ident)
@@ -396,14 +477,10 @@ impl Context {
                             let head = Expr::parse(lexer)?;
                             expect_token_kind(lexer, TokenKindSet::single(TokenKind::Equals))?;
                             let body = Expr::parse(lexer)?;
-                            anonymous_rule.insert(Rule {
-                                loc: token.loc,
-                                head,
-                                body,
-                            })
+                            anonymous_rule.insert(Rule {loc: token.loc, head, body})
                         }
 
-                        _ => unreachable!("Expected {} but got {}", expected_kinds, token.kind),
+                        _ => unreachable!("Expected {} but got {}", expected_kinds, token.kind)
                     };
 
                     // todo!("Throw an error if not a single match for the rule was found")
@@ -412,18 +489,11 @@ impl Context {
                         "all" => rule.apply(&expr, &mut ApplyAll),
                         "first" => rule.apply(&expr, &mut ApplyFirst),
                         "deep" => rule.apply(&expr, &mut ApplyDeep),
-                        _ => {
-                            return Err(Error::UnknownStrategy(
-                                strategy_name.text,
-                                strategy_name.loc,
-                            ))
-                        }
+                        _ => return Err(Error::UnknownStrategy(strategy_name.text, strategy_name.loc))
                     };
                     println!(" => {}", &new_expr);
                     self.shaping_history.push(
-                        self.current_expr
-                            .replace(new_expr)
-                            .expect("current_expr must have something"),
+                        self.current_expr.replace(new_expr).expect("current_expr must have something")
                     );
                 } else {
                     return Err(Error::NoShapingInPlace(keyword.loc));
@@ -434,7 +504,7 @@ impl Context {
                     self.current_expr = None;
                     self.shaping_history.clear();
                 } else {
-                    return Err(Error::NoShapingInPlace(keyword.loc));
+                    return Err(Error::NoShapingInPlace(keyword.loc))
                 }
             }
             TokenKind::Undo => {
@@ -443,19 +513,16 @@ impl Context {
                         println!(" => {}", &previous_expr);
                         self.current_expr.replace(previous_expr);
                     } else {
-                        return Err(Error::NoHistory(keyword.loc));
+                        return Err(Error::NoHistory(keyword.loc))
                     }
                 } else {
-                    return Err(Error::NoShapingInPlace(keyword.loc));
+                    return Err(Error::NoShapingInPlace(keyword.loc))
                 }
             }
             TokenKind::Quit => {
                 self.quit = true;
             }
-            _ => unreachable!(
-                "Expected {} but got {} '{}'",
-                expected_tokens, keyword.kind, keyword.text
-            ),
+            _ => unreachable!("Expected {} but got {} '{}'", expected_tokens, keyword.kind, keyword.text),
         }
         Ok(())
     }
@@ -463,7 +530,7 @@ impl Context {
 
 fn eprint_repl_loc_cursor(prompt: &str, loc: &Loc) {
     assert!(loc.row == 1);
-    eprintln!("{:>width$}^", "", width = prompt.len() + loc.col - 1);
+    eprintln!("{:>width$}^", "", width=prompt.len() + loc.col - 1);
 }
 
 fn main() {
@@ -479,10 +546,8 @@ fn main() {
             if let Err(err) = context.process_command(&mut lexer) {
                 match err {
                     Error::UnexpectedToken(expected_kinds, actual_token) => {
-                        eprintln!(
-                            "{}: ERROR: expected {} but got {} '{}'",
-                            actual_token.loc, expected_kinds, actual_token.kind, actual_token.text
-                        );
+                        eprintln!("{}: ERROR: expected {} but got {} '{}'",
+                                  actual_token.loc, expected_kinds, actual_token.kind, actual_token.text);
                     }
                     Error::RuleAlreadyExists(name, new_loc, old_loc) => {
                         eprintln!("{}: ERROR: redefinition of existing rule {}", new_loc, name);
@@ -502,10 +567,7 @@ fn main() {
                         eprintln!("{}: ERROR: no history", loc);
                     }
                     Error::UnknownStrategy(name, loc) => {
-                        eprintln!(
-                            "{}: ERROR: unknown rule application strategy '{}'",
-                            loc, name
-                        );
+                        eprintln!("{}: ERROR: unknown rule application strategy '{}'", loc, name);
                     }
                 }
                 std::process::exit(1);
@@ -530,16 +592,12 @@ fn main() {
             stdin().read_line(&mut command).unwrap();
             let mut lexer = Lexer::new(command.trim().chars(), None);
             if lexer.peek_token().kind != TokenKind::End {
-                let result = context.process_command(&mut lexer).and_then(|()| {
-                    expect_token_kind(&mut lexer, TokenKindSet::single(TokenKind::End))
-                });
+                let result = context.process_command(&mut lexer)
+                    .and_then(|()| expect_token_kind(&mut lexer, TokenKindSet::single(TokenKind::End)));
                 match result {
                     Err(Error::UnexpectedToken(expected, actual)) => {
                         eprint_repl_loc_cursor(prompt, &actual.loc);
-                        eprintln!(
-                            "ERROR: expected {} but got {} '{}'",
-                            expected, actual.kind, actual.text
-                        );
+                        eprintln!("ERROR: expected {} but got {} '{}'", expected, actual.kind, actual.text);
                     }
                     Err(Error::RuleAlreadyExists(name, new_loc, _old_loc)) => {
                         eprint_repl_loc_cursor(prompt, &new_loc);
@@ -573,6 +631,7 @@ fn main() {
     }
 }
 
+// TODO: Special mode for testing parsing of the expressions
 // TODO: Rule with several match clauses
 //
 // ```
