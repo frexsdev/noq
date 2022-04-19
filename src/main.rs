7352ceef26803230b2bd6cb02db75a7606082fef
ColaxDev <colax.dev@gmail.com>
2022-04-19 17:47:19 +0200

    Update TODOs

--- src/main.rs
+++ src/main.rs
@@ -1432,6 +1432,7 @@ impl<'a> fmt::Display for HighlightedSubexpr<'a> {
                 color::Fg(color::Reset)
             )
         } else {
+            // TODO: get rid of duplicate code in fmt::Display instance of HighlightedSubexpr and Expr
             match expr {
                 Expr::Sym(name) | Expr::Var(name) => write!(f, "{}", name),
                 Expr::Fun(head, args) => {
@@ -1563,4 +1564,3 @@ fn main() {
 }
 
 // TODO: Custom arbitrary operators like in Haskell
-// TODO: Autosuggestions in REPL
