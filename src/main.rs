3504f6a5801b8551f392a8f89f33761656355e14
ColaxDev <colax.dev@gmail.com>
2022-04-19 17:45:53 +0200

    Move NewCoolRepl to a separate module

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ use std::io;
 use std::io::Write;
 use std::io::{stdin, stdout};
 
-use termion::clear;
 use termion::color;
-use termion::cursor;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 #[macro_use]
 mod lexer;
+mod repl;
 
 use lexer::*;
+use repl::*;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Op {
@@ -1484,102 +1484,6 @@ impl<'a> fmt::Display for HighlightedSubexpr<'a> {
                 }
             }
         }
-    }
-}
-
-#[derive(Default)]
-struct NewCoolRepl {
-    buffer: Vec<char>,
-    cursor: usize,
-    popup: Vec<String>,
-}
-
-impl NewCoolRepl {
-    fn clear(&mut self) {
-        self.buffer.clear();
-        self.cursor = 0;
-    }
-
-    fn take(&mut self) -> String {
-        let result = self.buffer.iter().collect();
-        self.clear();
-        result
-    }
-
-    fn insert_char(&mut self, x: char) {
-        self.buffer.insert(self.cursor, x);
-        self.cursor += 1;
-    }
-
-    fn backspace(&mut self) {
-        if self.cursor > 0 {
-            self.buffer.remove(self.cursor - 1);
-            self.cursor -= 1;
-        }
-    }
-
-    fn home(&mut self) {
-        self.cursor = 0;
-    }
-
-    fn end(&mut self) {
-        self.cursor = self.buffer.len();
-    }
-
-    fn left_word(&mut self) {
-        while self.cursor > 0
-            && self.cursor <= self.buffer.len()
-            && !self.buffer.get(self.cursor - 1).unwrap().is_alphanumeric()
-        {
-            self.cursor -= 1;
-        }
-        while self.cursor > 0
-            && self.cursor <= self.buffer.len()
-            && self.buffer.get(self.cursor - 1).unwrap().is_alphanumeric()
-        {
-            self.cursor -= 1;
-        }
-    }
-
-    fn right_word(&mut self) {
-        while self.cursor < self.buffer.len()
-            && !self.buffer.get(self.cursor).unwrap().is_alphanumeric()
-        {
-            self.cursor += 1;
-        }
-        while self.cursor < self.buffer.len()
-            && self.buffer.get(self.cursor).unwrap().is_alphanumeric()
-        {
-            self.cursor += 1;
-        }
-    }
-
-    fn left_char(&mut self) {
-        if self.cursor > 0 {
-            self.cursor -= 1;
-        }
-    }
-
-    fn right_char(&mut self) {
-        if self.cursor < self.buffer.len() {
-            self.cursor += 1;
-        }
-    }
-
-    fn render(&self, prompt: &str, sink: &mut impl Write) -> io::Result<()> {
-        const POPUP_SIZE: usize = 5;
-        let buffer: String = self.buffer.iter().collect();
-        write!(sink, "\r{}{}{}\r\n", clear::AfterCursor, prompt, &buffer)?;
-        for line in self.popup.iter().take(POPUP_SIZE) {
-            write!(sink, "{}\r\n", line)?;
-        }
-        write!(
-            sink,
-            "{}{}",
-            cursor::Up((POPUP_SIZE.min(self.popup.len()) + 1).try_into().unwrap()),
-            cursor::Right((prompt.len() + self.cursor).try_into().unwrap())
-        )?;
-        Ok(())
     }
 }
 
